0a17b2af7840db741898ae4778f52d9dc8ed80f6
Satish Talluri <satish@neptune.io>
2015-01-15 19:11:35 -0800

    Remove license key

--- newrelic.js
+++ newrelic.js
@@ -12,7 +12,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key : '80adf7586b43d0e710c7e85895a2004dedd9604d',
+  license_key : '',
   logging : {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
