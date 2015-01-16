5bb4d9bb4cbabe685855ec2ed0e34e3ff0f7bdaa
Satish Talluri <satish@neptune.io>
2015-01-15 19:09:43 -0800

    Add node server with newrelic integrated

---
+++ server.js
@@ -0,0 +1,35 @@
+var newrelic = require('newrelic');
+
+var http = require('http');
+
+//JSON
+var jsonToSend = {
+  Name:"John Art",
+  Email: "john.art@johnart.com",
+  Address:"Infinite loop, cupertino",
+  Role:"Adminstrator"
+};
+
+//Server
+http.createServer(function(req, res){
+  switch(req.url) {
+    case '/5000' :
+      setTimeout(function(){
+      res.writeHead(200, {'Content-Type':'application/json'});
+      res.end(JSON.stringify(jsonToSend));
+      },5000);
+    break;
+    case '/error' :
+      res.writeHead(500, {'Content-Type':'text/plain'});
+    res.end("500 Error generated dude!");
+    break;
+    default :
+      res.writeHead(200, {'Content-Type':'application/json'});
+    res.end(JSON.stringify(jsonToSend));
+    break;
+  }
+
+}).listen(8080,'127.0.0.1');
+
+console.log('New Relic test server just started!');
+
